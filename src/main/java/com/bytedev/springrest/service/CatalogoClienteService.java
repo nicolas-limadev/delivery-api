e884111c3a65592d40ec153df4204349a06d908d
nicolas-limadev <nicolasbasto76@gmail.com>
2023-05-03 23:15:45 -0300

    correction

--- src/main/java/com/bytedev/springrest/service/CatalogoClienteService.java
+++ src/main/java/com/bytedev/springrest/service/CatalogoClienteService.java
@@ -1,13 +1,11 @@
 package com.bytedev.springrest.service;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.bytedev.springrest.expection.NegocioException;
 import com.bytedev.springrest.model.Cliente;
 import com.bytedev.springrest.repository.ClienteRepository;
-
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @AllArgsConstructor
 @Service
@@ -16,18 +14,22 @@ public class CatalogoClienteService {
   private ClienteRepository clienteRepository;
 
   public Cliente buscar(Long clienteId) {
-    return clienteRepository.findById(clienteId)
-        .orElseThrow(() -> new NegocioException("Cliente não encontrado"));
+    return clienteRepository
+      .findById(clienteId)
+      .orElseThrow(() -> new NegocioException("Cliente não encontrado"));
   }
 
   @Transactional
   public Cliente salvar(Cliente cliente) {
-
-    boolean emailEmUso = clienteRepository.findByEmail(cliente.getEmail()).stream()
-        .anyMatch(clienteExistente -> !clienteExistente.equals(cliente));
+    boolean emailEmUso = clienteRepository
+      .findByEmail(cliente.getEmail())
+      .stream()
+      .anyMatch(clienteExistente -> !clienteExistente.equals(cliente));
 
     if (emailEmUso) {
-      throw new NegocioException("Já existe um cliente cadastrado com este e-mail.");
+      throw new NegocioException(
+        "Já existe um cliente cadastrado com este e-mail."
+      );
     }
 
     return clienteRepository.save(cliente);
@@ -35,11 +37,10 @@ public class CatalogoClienteService {
 
   @Transactional
   public void excluir(Long clientId) {
-
-    clienteRepository.findById(clientId)
-        .orElseThrow(() -> new NegocioException("ID não encontrado"));
+    clienteRepository
+      .findById(clientId)
+      .orElseThrow(() -> new NegocioException("ID não encontrado"));
 
     clienteRepository.deleteById(clientId);
-
   }
 }
